cac18f6d0099928048b97bf913bf71fe1be7e091
loliver42 <156827618+loliver42@users.noreply.github.com>
2024-02-06 09:54:30 -0500

    Added date js

--- src/script.js
+++ src/script.js
@@ -19,6 +19,20 @@ function updateWeather(response) {
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}  ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
