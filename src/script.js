15fac7bda1b7bd82a1c6354e6da7cee1c25b3447
loliver42 <156827618+loliver42@users.noreply.github.com>
2024-02-06 10:31:05 -0500

    added js weather

--- src/script.js
+++ src/script.js
@@ -7,14 +7,16 @@ function updateWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-  console.log(response.data);
+  let iconElement = document.querySelector("#icon");
   cityElement.innerHTML = response.data.city;
 
   descriptionElement.innerHTML = response.data.condition.description;
   timeElement.innerHTML = formatDate(date);
+
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src= "${response.data.condition.icon_url}" class="weather-icon"/>`;
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
