a014602d3bfb98d58fb1284bcac7650d1133a8b6
loliver42 <156827618+loliver42@users.noreply.github.com>
2024-02-06 09:41:02 -0500

    Added js description functions

--- src/script.js
+++ src/script.js
@@ -2,9 +2,23 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  console.log(response.data);
   cityElement.innerHTML = response.data.city;
+
+  descriptionElement.innerHTML = response.data.condition.description;
+  timeElement.innerHTML = formatDate(date);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
 }
 
 function searchCity(city) {
